9b93cde2acd34f678d26218dd755cca3c8d215d9
dougclutter_cp <SND\dougclutter_cp>
2011-11-27 21:07:31 +0000

    Changed ProxyServer: 1) Added StartListening/StopListening methods 2) Removed code from constructor that started the listening process - constructors shouldn't start things 3) Removed IsListening - never used 4) Added IsDisposed - checking in StartListening to be sure it isn't called after object is disposed 5) Started using TcpListener.Pending() method so we don't need to "break" into the AcceptSocket call 6) Added catch for IOException because this was occurring when we failed to send email to an invalid address 7) Moved code that closes the socket into ProxyServer class because it is the class that opens the socket 8) Added more tracing 9) Added handler for generic exceptions that logs them and then rethrows them 10) Deleted AcceptSocketState class because it was not needed

--- Src/SmtpProxy/ProxyServer.cs
+++ Src/SmtpProxy/ProxyServer.cs
@@ -2,8 +2,10 @@
  * Licensed for use under the Microsoft Public License which is included by reference here.
  */
 using System;
-using System.Threading;
+using System.Diagnostics;
+using System.IO;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 using SmtpProxy.Properties;
 
@@ -16,22 +18,31 @@ namespace SmtpProxy
     {
         #region Variables
         readonly TcpListener Listener;
-        readonly CancellationTokenSource TokenSource = new CancellationTokenSource();
+        readonly CancellationTokenSource TokenSource;
+        bool IsDisposed;
         #endregion
-
         #region Constructor
         public ProxyServer()
         {
+            // Initialize cancellation token and listener
+            TokenSource = new CancellationTokenSource();
             Listener = new TcpListener(System.Net.IPAddress.Any, Settings.Default.PortToListenOn);
-            Task.Factory.StartNew(() => Listen(TokenSource.Token), TokenSource.Token);
         }
         #endregion
-
-        #region Properties
-        public bool IsListening { get; private set; }
-        #endregion
-
+        
         #region Public Methods
+        public void StartListening()
+        {
+            if (IsDisposed)
+                throw new InvalidOperationException("ProxyServer.StartListening() cannot be called after the object has been Disposed or StopListening has been called.");
+
+            // Start on a new thread so StartListening can return immediately
+            Task.Factory.StartNew(() => Listen());
+        }
+        public void StopListening()
+        {
+            Dispose();
+        }
         public void Dispose()
         {
             Dispose(true);
@@ -40,73 +51,104 @@ namespace SmtpProxy
         #endregion
 
         #region Protected / Private Methods
-        void Listen(CancellationToken token)
+        void Listen()
         {
-            Program.Trace.TraceInformation("Listen started");
-            try
-            {
-                // Start the listener
-                Listener.Start();
-                IsListening = true;
+            // Start the listener
+            Listener.Start();
+            Program.Trace.TraceInformation("Listener started. Connections on port {0} will be forwarded to {1}:{2}", 
+                Settings.Default.PortToListenOn,
+                Settings.Default.SmtpHostUrl,
+                Settings.Default.SmtpPort);
 
-                // Wait until a client tries to connect to the listener
-                var state = new AcceptSocketState { Token = token };
-                Task.Factory.StartNew(() => AcceptSocket(state), state.Token);
-
-                // Run until cancelled
-                token.WaitHandle.WaitOne();
-            }
-            finally
+            // Loop until cancellation is requested
+            while (!TokenSource.Token.IsCancellationRequested)
             {
-                Program.Trace.TraceInformation("Listen ended");
-                IsListening = false;
+                AcceptSocket();
             }
         }
-        void AcceptSocket(AcceptSocketState state)
+        void AcceptSocket()
         {
-            Socket socket = null;
             try
             {
-                // Wait until a client connects
-                socket = Listener.AcceptSocket();
+                // Sleep until a request is pending OR the request has been cancelled
+                while (!Listener.Pending() && !TokenSource.Token.IsCancellationRequested)
+                {
+                    Thread.Sleep(500);
+                }
 
-                // Make sure we weren't cancelled
-                state.Token.ThrowIfCancellationRequested();
+                // If we have a pending request AND we have NOT been cancelled,
+                // process the pending request in a new Task.
+                if (Listener.Pending() && !TokenSource.Token.IsCancellationRequested)
+                {
+                    // Accept the pending connection.
+                    // The socket will be closed in ProcessSocket.
+                    Socket socket = Listener.AcceptSocket();
 
-                // Process the socket on a new Task
-                var processor = new SocketProcessor();
-                Task.Factory.StartNew(() => processor.Start(socket, state.Token), state.Token);
-            }
-            catch (SocketException ex)
-            {
-                Program.Trace.TraceInformation("ProxyServer.AcceptSocket has ignored a SocketException ({0}) probably because the TcpListener was stopped.", ex.Message);
+                    // Process the socket request in a new task so we can start waiting for the next request before the current is done processing
+                    Task.Factory.StartNew(() => ProcessSocket(socket));
+                }
             }
             catch (InvalidOperationException ex)
             {
                 // Listener not started or has been stopped
-                Program.Trace.TraceInformation("ProxyServer.AcceptSocket has ignored an InvalidOperationException ({0}) probably because the TcpListener was stopped.", ex.Message);
+                Program.Trace.TraceInformation("ProxyServer.AcceptSocket has ignored an InvalidOperationException ({0}).", ex.Message);
+            }
+        }
+        void ProcessSocket(Socket socket)
+        {
+            try
+            {
+                // Process the socket
+                var processor = new SocketProcessor();
+                processor.Start(socket, TokenSource.Token);
+            }
+            catch (SocketException ex)
+            {
+                // Listener may have been stopped
+                Program.Trace.TraceInformation("ProxyServer.ProcessSocket has ignored a SocketException ({0}).", ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                // Listener not started or has been stopped
+                Program.Trace.TraceInformation("ProxyServer.ProcessSocket has ignored an InvalidOperationException ({0}).", ex.Message);
+            }
+            catch (IOException ex)
+            {
+                // Failed to complete a read operation
+                Program.Trace.TraceInformation("ProxyServer.ProcessSocket has ignored an IOException ({0}).", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Log unexpected exception
+                Program.Trace.TraceEvent(TraceEventType.Error, 3,
+                    "ProxyServer.ProcessSocket has thrown an unexpected Exception: {0}: {1}", ex.GetType().Name, ex.Message);
+                throw;
             }
             finally
             {
-                // Start listening for another client
-                state.Token.ThrowIfCancellationRequested();
-                Task.Factory.StartNew(() => AcceptSocket(state), state.Token);
+                if (socket != null)
+                {
+                    socket.Close();
+                    Program.Trace.TraceInformation("Socket {0} closed", socket.Handle);
+                }
             }
         }
         protected void Dispose(bool disposing)
         {
             if (disposing)
             {
-                TokenSource.Cancel();
-                Listener.Stop();
-            }
-        }
-        #endregion
+                if (!IsDisposed)
+                {
+                    IsDisposed = true;
 
-        #region Internal Classes
-        class AcceptSocketState
-        {
-            public CancellationToken Token;
+                    // Cancel TokenSource so child tasks will exit gracefully
+                    TokenSource.Cancel();
+
+                    // Stop the listener so no further input will be received and processed
+                    Listener.Stop();
+                    Program.Trace.TraceInformation("Listener on port {0} stopped ", Settings.Default.PortToListenOn);
+                }
+            }
         }
         #endregion
     }
