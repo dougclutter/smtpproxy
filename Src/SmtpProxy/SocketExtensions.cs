37d48ceff26f8503694b1d7fd733a7b5bda2340d
dougclutter_cp <SND\dougclutter_cp>
2011-11-30 18:04:28 +0000

    Added SocketExtensions

---
+++ Src/SmtpProxy/SocketExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace SmtpProxy
+{
+    // Thanks to http://stackoverflow.com/questions/722240/instantly-detect-client-disconnection-from-server-socket
+    static class SocketExtensions
+    {
+        public static bool IsConnected(this Socket socket)
+        {
+            try
+            {
+                return !(socket.Poll(1, SelectMode.SelectRead) && socket.Available == 0);
+            }
+            catch (SocketException) { return false; }
+        }
+    }
+}
